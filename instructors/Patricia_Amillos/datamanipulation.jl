650227b0e8c610ae90f33b43f00b4f430f540917
Douglas Bates <dmbates@gmail.com>
2021-09-13 10:45:13 +0200

    Update datamanipulation script and result

--- instructors/Patricia_Amillos/datamanipulation.jl
+++ instructors/Patricia_Amillos/datamanipulation.jl
@@ -1,10 +1,16 @@
 using Arrow, Chain, CSV, DataFrameMacros, DataFrames
+      # view this dataframe in table viewer after reading
+axb_data = CSV.read("./Patricia_Amillos/full_axb.csv", DataFrame; missingstrings="NA");
 
-axb_data = CSV.read("../../Patricia_Amillos/full_axb.csv", DataFrame; missingstrings="NA"); # view in table viewer
+      # a brief summary to check on datatypes in columns
+describe(axb_data)
 
       # check for columns that are constant - drop them and Column1, which is row numbers
-constants = names(axb_data, nm -> isone(length(unique(axb_data[:, nm]))))
+constants = names(axb_data, nm -> isone(length(unique(axb_data[!, nm]))))
+
+      # reduce the dataframe and transform columns using DataFrameMacros
 axb_reduced = @chain axb_data begin
+            # drop the constant columns and Column1, which is just the row numbers
     select(Not(push!(constants, "Column1")))
     @transform!(
         :accuracy = :response == :answer_key, # evaluate accuracy as Boolean
@@ -13,8 +19,10 @@ axb_reduced = @chain axb_data begin
         :x_dental = :x_dental == 1,
     )
 end
-Arrow.write(
-    "./data/axb_data.arrow",
+
+      # write the transformed data as an Arrow file using Zstd compression
+arrowfile = Arrow.write(
+    "./instructors/Patricia_Amillos/data/axb_data.arrow",
     select(   # re-order the columns so the important ones are first
         axb_reduced,
         :ID,
@@ -23,5 +31,12 @@ Arrow.write(
         :Task,
         :trained,
         :);
-    compress = :zstd
+    compress = :zstd,
     )
+      # check how large the Arrow file is
+filesize(arrowfile)
+
+      # as a check, read in the Arrow file and check the column types
+axb_arrow = DataFrame(Arrow.Table(arrowfile))
+describe(axb_arrow)
+
